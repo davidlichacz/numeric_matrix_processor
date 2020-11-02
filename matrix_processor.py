ed3387ad2e33cd05838b10f4a4f44d6ffcc45d80
David Lichacz <dlichacz@gmail.com>
2020-11-02 16:46:26 -0500

    Completed Stage 1

---
+++ matrix_processor.py
@@ -0,0 +1,31 @@
+class Matrix():
+    def __init__(self):
+        self.dimensions = None
+        self.values = None
+
+    def get_dimensions(self):
+        self.dimensions = [int(dim) for dim in input().split()]
+
+    def get_values(self):
+        self.values = [[int(num) for num in input().split()] for i in range(self.dimensions[0])]
+
+    def add_matrix(self, second_matrix):
+        if self.dimensions != second_matrix.dimensions:
+            print('ERROR')
+        else:
+            zipped = list(zip(self.values, second_matrix.values))
+            pairs = [list(zip(a, b)) for (a, b) in zipped]
+            summed = [list(map(sum, pair)) for pair in pairs]
+            for row in summed:
+                print(*row)
+
+
+matrix_a = Matrix()
+matrix_a.get_dimensions()
+matrix_a.get_values()
+
+matrix_b = Matrix()
+matrix_b.get_dimensions()
+matrix_b.get_values()
+
+matrix_a.add_matrix(matrix_b)
